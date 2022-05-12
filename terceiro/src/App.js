6e9db88771dc1adbda4cbbd8e5af3736a93f5f33
JOAO VITOR DA SILVA SOUZA <joao.vssouza42@senacsp.edu.br>
2022-05-12 17:29:00 -0300

    Aprendendo sobre estados

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,13 +1,55 @@
 import {useState} from "react";
 function App() {
 
-const [numero, setNumero] = useState(1);
-  return (
-    <div>
-      <p>O número atua é {numero}</p>
-        <p>
-          <button title="Clique" onClick={() => setNumero(numero + 1)}> Clique aqui </button>
-        </p>
+const [produto, setProduto] = useState(
+  [
+    {
+    id:500,
+    nome:"Mouse",
+    categoria:"informatica",
+    preco:50.50
+    },
+    {
+    id:501,
+    nome:"Teclado",
+    categoria:"informatica",
+    preco:150.90
+    }
+  ]
+);
+return (
+<div>
+  <button onClick={()=> {
+    let id = prompt("Entre com id do produto");
+    let nome = prompt("Entre com nome do produto");
+    let categoria = prompt("Entre com categoria do produto");
+    let preco = prompt("Entre com o preço do produto");
+    
+    setProduto(
+      produto.concat({
+      id: id,
+      nome: nome,
+      categoria: categoria,
+      preco: preco,
+    }),
+  )
+}}>
+  Adicionar produto
+ </button>
+  
+  <h2>Produtos Cadastrados</h2>
+
+    {produto.map((item,index) => (
+      <div key={index}>
+      <h3>{item.nome}</h3>
+      <ul>
+        <li>Código do Produto: {item.id}</li>
+        <li>Categoria do Produto: {item.categoria}</li>
+        <li>Preço do Produto: R$ {item.preco}</li>
+      </ul>
+      </div>
+    ))}
+
     </div>
   );
 }
