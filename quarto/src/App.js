1ae8814b8b1caae4c20af4ad696d98a5d93f847b
JOAO VITOR DA SILVA SOUZA <joao.vssouza42@senacsp.edu.br>
2022-05-17 17:27:15 -0300

    criação da pasta quarto

---
+++ quarto/src/App.js
@@ -0,0 +1,14 @@
+import Superior from "./components/header/Superior";
+import Container from "./components/main/Container";
+import "./css/style.css";
+
+function App() {
+  return (
+    <div>
+      <Superior />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ quarto/src/components/main/Container.js
@@ -0,0 +1,38 @@
+import Mensagem from "./Mensagem";
+import Conteudo from "./Conteudo";
+import {useState, useEffect} from "react";
+
+export default function Container() {
+//vamos criar o estado inicial dos produtos, ou seja,
+//quais dados existem dentro do objeto produtos
+
+const [produtos, setProdutos] = useState([
+  {
+    id:"",
+    nome:"",
+    descricao:"",
+    categoria:"",
+    preco:"",
+    msg:[]
+  }
+]);
+
+useEffect(() => {
+  fetch("http://10.26.49.56:4000/api/services/produto/listar")
+  .then((response) => response.json())
+  .then((dados) =>{
+    setProdutos(dados.output);
+  })
+  .catch((erro) => console.error(`Erro ao carregar a api ->${erro}`))
+}, [])
+
+
+
+
+  return (
+    <div className="container">
+      <Mensagem />
+      <Conteudo dados = {produtos} />
+    </div>
+  );
+}
